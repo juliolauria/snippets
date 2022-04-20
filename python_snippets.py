3aa2def81d89dbcfa0cf8ca14465e8f6141e3775
juliolauria <48496104+juliolauria@users.noreply.github.com>
2022-04-20 15:20:29 -0300

    Execution time snippet

---
+++ python_snippets.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+#####
+# Execution time
+#####
+
+startTime = datetime.now()
+
+# code here
+
+print("tempo de execução:" round((datetime.now() - startTime)*60), 2)
