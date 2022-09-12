42711a39a8e7fff7f26e857b4390b397a5730a03
Julio Vince <48496104+juliolauria@users.noreply.github.com>
2022-09-12 14:53:28 -0300

    Update pandas_snippets.py

--- pandas_snippets.py
+++ pandas_snippets.py
@@ -2,3 +2,6 @@ import pandas as pd
 
 pd.options.display.max_columns = 99
 pd.options.mode.chained_assignment = None
+
+# Insert a column in a custom location in df
+df.insert(1, 'Col', list_of_values) # Insert list_of_values with name 'Col' in second position
