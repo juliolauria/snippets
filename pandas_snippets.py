4445356e2ba5eb04020ea15d760a29ce217e6a36
Julio Vince Lauria <48496104+juliolauria@users.noreply.github.com>
2024-01-05 17:11:17 +0100

    Update pandas_snippets.py
    pd.cut

--- pandas_snippets.py
+++ pandas_snippets.py
@@ -5,3 +5,8 @@ pd.options.mode.chained_assignment = None
 
 # Insert a column in a custom location in df
 df.insert(1, 'Col', list_of_values) # Insert list_of_values with name 'Col' in second position
+
+# Create bins based on intervals ([pd.cut)
+bins = [-999, 11, 12, 24, 999]
+labels = ['A', 'B', 'C', 'D']
+df['ContractDuration'] = pd.cut(df['col'], bins=bins, labels=labels)
