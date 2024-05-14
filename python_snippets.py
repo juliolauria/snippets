b2863ae03b5bc5fd107907621f2780ce90ef1507
Julio <julio@bluewalnut.com>
2024-05-14 15:57:59 +0200

    Python open files and git remove merged branch commands

--- python_snippets.py
+++ python_snippets.py
@@ -8,8 +8,32 @@ startTime = datetime.now()
 
 # code here
 
-print("tempo de execução: " + str(datetime.now() - startTime).split('.')[0])
+print("Execution time: " + str(datetime.now() - startTime).split('.')[0])
 
 # Ignore warnings
 
 warnings.filterwarnings("ignore", module="lightgbm")
+
+###
+# Debug
+###
+
+import pdb
+pdb.set_trace()
+
+# get code and move to a new python session
+# !import code; code.interact(local=vars())
+
+# Save data to JSON
+with open("temp/dev_masterdata/masterdata_cvt.json", 'w') as json_file:
+    json.dump(master_data, json_file, indent=4)
+
+# Load data from JSON file
+with open("temp/dev_masterdata/masterdata_cvt.json", 'r') as json_file:
+    master_data = json.load(json_file)
+
+# Save Master Data
+import json
+company_id = 'cl'
+with open(f"temp/dev_masterdata/masterdata_{company_id}.json", 'w') as json_file:
+    json.dump(master_data, json_file)
